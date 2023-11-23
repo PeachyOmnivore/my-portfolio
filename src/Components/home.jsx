bf0c3538ea1b0ea11da46537d741a752867f1eb7
Lukas Dembicki <lad_777@hotmail.com>
2023-11-23 15:32:24 +0000

    Changed some text on home

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -2,12 +2,12 @@ export default function Home() {
     return (
         <>
             <div className=" main backgroundImageTrees "></div>
-            <section className="main introduction home-container">
-                <article>
-                    <h1>WELCOME TO MY PORTFOLIO</h1>
+            <section className="main home-container">
+                <article className="introduction">
+                    <h1>WELCOME</h1>
                     <br />
-                    <p>I have built this portfolio to showcase some of my skills as a junior software engineer. <br />
-                        I enjoy keeping things simple and stylized.</p>
+                    <h2>My name is Lukas Allen Dembicki</h2><br />
+                    <p>I have built this portfolio to showcase some of my skills as a junior software engineer.</p>
                 </article>
             </section>
         </>
