e2474ed6f759eda99b0ba4d5bf2e20bfb777b1a7
Lukas Dembicki <lad_777@hotmail.com>
2023-11-23 17:47:47 +0000

    Getting layout done for projects page

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -1,5 +1,22 @@
 export default function Projects() {
     return (
-        <p className="main">This is the projects page</p>
+        <>
+            <div className=" main backgroundImageTech "></div>
+            <section className="main projects-container">
+                <div className="card">
+                    <p>CARD 1</p>
+                </div>
+                <div className="card">
+                    <p>CARD 2</p>
+                </div>
+                <div className="card">
+                    <p>CARD 3</p>
+                </div>
+                <div className="card">
+                    <p>CARD 4</p>
+                </div>
+            </section>
+            
+        </>
     )
 }
