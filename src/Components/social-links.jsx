8d56f3cda25673c26d6689bd26d08db566285a65
Lukas Dembicki <lad_777@hotmail.com>
2023-11-23 13:27:54 +0000

    positions improved.

--- src/Components/social-links.jsx
+++ src/Components/social-links.jsx
@@ -3,17 +3,17 @@ import {GitHub, Email, Instagram, LinkedIn} from "../assets/icons.jsx"
 export default function Socials() {
     return (
         <nav>
-            <ul>
-                <li className="socialLinks">
+            <ul className="socialsNav">
+                <li>
                     <a href="#"><img className="svg" src={GitHub} alt="GitHub" /></a>
                 </li>
-                <li className="socialLinks">
+                <li>
                     <a href="#"><img className="svg" src={LinkedIn} alt="LinkedIn" /></a>
                 </li>
-                <li className="socialLinks">
+                <li>
                     <a href="#"><img className="svg" src={Instagram} alt="Instagram" /></a>
                 </li>
-                <li className="socialLinks">
+                <li>
                     <a href="#"><img className="svg" src={Email} alt="Email" /></a>
                 </li>
             </ul>
