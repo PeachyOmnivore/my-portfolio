832215edd5c268c7cd0d8d3bbc83da9d0a5bea03
Lukas Dembicki <lad_777@hotmail.com>
2023-11-23 15:25:39 +0000

    first home card built

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,5 +1,17 @@
 export default function Home() {
     return (
-        <p className="main">This is the home page</p>
+        <>
+            <div className=" main backgroundImageTrees "></div>
+            <section className="main introduction home-container">
+                <article>
+                    <h1>WELCOME TO MY PORTFOLIO</h1>
+                    <br />
+                    <p>I have built this portfolio to showcase some of my skills as a junior software engineer. <br />
+                        I enjoy keeping things simple and stylized.</p>
+                </article>
+            </section>
+        </>
+
+
     )
 }
