caf846c31593cfb792cbbd32563a86938a71c9b4
Lukas Dembicki <lad_777@hotmail.com>
2023-11-24 13:39:28 +0000

    Form created. No functionality.

--- src/Components/message-me.jsx
+++ src/Components/message-me.jsx
@@ -1,5 +1,32 @@
 export default function MessageMe() {
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+}
+
     return (
-        <p className="main">This is the message page</p>
+        <>
+            <div className=" main backgroundImagePapers "></div>
+            <section className="main form-container">
+                <form onSubmit={handleSubmit}className="messageForm">
+                    <h1>SEND ME A MESSAGE</h1>
+                    <div>
+                        <label> Your Name:</label>
+                        <input type="text" name="name" placeholder="Enter your name" />
+                        <hr />
+                    </div>
+                    <div>
+                        <label>Email:</label>
+                        <input type="email" name="email" placeholder="Enter your email" />
+                        <hr />
+                    </div>
+                    <div>
+                        <label>Your message:</label>
+                        <textarea type="textbox" name="message" />
+                    </div>
+                    <input className="submitButton" type="submit" name="submit" />
+                </form>
+            </section>
+        </>
     )
 }
