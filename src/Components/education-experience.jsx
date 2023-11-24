85343d5fe99bcb1dc9258a668be3cfbec34a462a
Lukas Dembicki <lad_777@hotmail.com>
2023-11-24 11:35:38 +0000

    content added into education/experience

--- src/Components/education-experience.jsx
+++ src/Components/education-experience.jsx
@@ -1,5 +1,19 @@
 export default function EducationExperience() {
     return (
-        <p className="main">This is the education page</p>
+        <>
+            <div className=" main backgroundImageGrad "></div>
+            <section className="main education-container">
+                <article className="education">
+                    <h1>Education and Experience</h1>
+                    <br />
+                    <ul>
+                        <li>
+                            <h2>Boolean School of Code</h2>
+                            <p>2023/2024: 6 Month program covering both front-end development and back-end. Experience in group development and individual projects.</p>
+                        </li>
+                    </ul>
+                </article>
+            </section>
+        </>
     )
 }
