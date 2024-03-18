0fc66a99d62fa589894a7c7dcc4ca7dcc0ce9a2b
Lukas <lad_777@hotmail.com>
2024-03-18 16:15:01 +0000

    feat: Added wellGroundedCounselling

--- src/AllProjects.js
+++ src/AllProjects.js
@@ -1,42 +1,44 @@
+// Descriptions should not be longer than 
+
 const AllProjects = [
     {
         id: 1,
         name: "An Outdoor Adventure",
-        image: "https://images.pexels.com/photos/13450235/pexels-photo-13450235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "This is a project where I developed a choose your own adenture style application",
+        image: "./src/assets/images/anOutdoorAdventure.png",
+        description: "A project where I developed a choose your own adenture style application",
         gitRepo: "https://github.com/PeachyOmnivore/An-outdoor-adventure",
         website: "https://anoutdooradventure.netlify.app"
     },
     {
         id: 2,
-        name: "Goose",
-        image: "https://images.pexels.com/photos/57406/greater-snow-goose-goose-snow-goose-wading-bird-57406.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "This is a project where I posted a goose",
-        gitRepo: "github.com",
-        website: "https://www.google.com/search?q=goose&sca_esv=585139827&ei=VR9hZYmqEMjokgXKrI_QBg&ved=0ahUKEwjJqOvC092CAxVItKQKHUrWA2oQ4dUDCA8&uact=5&oq=goose&gs_lp=Egxnd3Mtd2l6LXNlcnAiBWdvb3NlMhAQABiABBiKBRixAxiDARhDMgsQABiABBixAxiDATIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIKEAAYgAQYigUYQzIQEAAYgAQYigUYsQMYgwEYQzIKEAAYgAQYigUYQzIOEAAYgAQYigUYsQMYgwEyChAAGIAEGIoFGEMyEBAuGIAEGIoFGLEDGNQCGENI_QVQAFj0BHAAeAGQAQCYAb0CoAHBBqoBBzAuMy4wLjG4AQPIAQD4AQHCAhAQLhiABBiKBRjHARjRAxhDwgIKEC4YgAQYigUYQ8ICHxAuGIAEGIoFGMcBGNEDGEMYlwUY3AQY3gQY4ATYAQHCAhYQLhiABBiKBRixAxiDARjHARjRAxhDwgINEAAYgAQYigUYsQMYQ8ICJRAuGIAEGIoFGLEDGIMBGMcBGNEDGEMYlwUY3AQY3gQY4ATYAQHiAwQYACBBiAYBugYGCAEQARgU&sclient=gws-wiz-serp"
+        name: "Well Grounded Counselling",
+        image: "./src/assets/images/wellGroundedCounselling.jpg",
+        description: "A website for a counsellor to advertise and book clients",
+        gitRepo: "https://github.com/PeachyOmnivore/WellGroundedCounselling",
+        website: "Coming soon"
     },
     {
         id: 3,
         name: "Bluebird",
         image: "https://images.pexels.com/photos/7014820/pexels-photo-7014820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "This is a project where I posted a bluebird",
-        gitRepo: "github.com",
+        description: "This is a placeholder where I posted a bluebird",
+        gitRepo: "https://www.github.com",
         website: "https://www.google.com/search?q=bluebird&sca_esv=585139827&ei=Zx9hZbycOtq_i-gP77uM4A8&ved=0ahUKEwj869_L092CAxXa3wIHHe8dA_wQ4dUDCA8&uact=5&oq=bluebird&gs_lp=Egxnd3Mtd2l6LXNlcnAiCGJsdWViaXJkMg4QLhiABBiKBRixAxiRAjIXEC4YgAQYigUYsQMYgwEYxwEYrwEYkQIyERAuGIAEGLEDGIMBGMcBGNEDMgUQABiABDIOEC4YgAQYsQMYxwEYrwEyCxAuGIAEGMcBGK8BMgUQABiABDILEC4YgAQYxwEYrwEyCxAuGIAEGMcBGK8BMgsQABiABBixAxjJAzIdEC4YgAQYigUYsQMYkQIYlwUY3AQY3gQY3wTYAQNIlRBQ8AZYsg9wA3gBkAEBmAHPAqAB8wuqAQcwLjMuMy4xuAEDyAEA-AEBwgIKEAAYRxjWBBiwA8ICDRAAGIAEGIoFGLADGEPCAg4QABjkAhjWBBiwA9gBAcICFhAuGIAEGIoFGNQCGMgDGLADGEPYAQLCAhMQLhiABBiKBRjIAxiwAxhD2AECwgIOEAAYgAQYigUYsQMYgwHCAgsQABiABBixAxiDAcICIBAuGIAEGLEDGIMBGMcBGNEDGJcFGNwEGN4EGOAE2AEDwgIQEC4YgAQYigUYxwEY0QMYQ8ICChAuGIAEGIoFGEPCAgoQABiABBiKBRhDwgINEC4YgAQYigUY1AIYQ8ICEBAuGIAEGIoFGMcBGK8BGEPCAh8QLhiABBiKBRjHARjRAxhDGJcFGNwEGN4EGOAE2AEDwgIREC4YgAQYigUYsQMYxwEYrwHCAggQLhiABBixA8ICDRAuGIAEGIoFGLEDGEPCAhAQLhiABBiKBRixAxiDARhD4gMEGAAgQYgGAZAGEroGBggBEAEYCboGBggCEAEYCLoGBggDEAEYFA&sclient=gws-wiz-serp"
     },
     {
         id: 4,
         name: "Chicken",
         image: "https://images.pexels.com/photos/195226/pexels-photo-195226.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "This is a project where I posted a Chicken",
-        gitRepo: "github.com",
+        description: "This is a placeholder where I posted a Chicken",
+        gitRepo: "https://www.github.com",
         website: "https://www.google.com/search?q=chicken+bird&sca_esv=585139827&ei=kx9hZZXjDtC9i-gP6dOtuA4&gs_ssp=eJzj4tDP1TewTDItMWD04knOyEzOTs1TSMosSgEAVP0Hbw&oq=chicken&gs_lp=Egxnd3Mtd2l6LXNlcnAiB2NoaWNrZW4qAggAMg0QLhjUAhiABBiKBRhDMg0QLhjUAhiABBiKBRhDMgoQABiABBiKBRhDMgoQABiABBiKBRhDMhAQLhiABBiKBRixAxiDARhDMgoQABiABBiKBRhDMggQABiABBixAzIKEC4YgAQYigUYQzIIEAAYgAQYsQMyCBAAGIAEGLEDMhwQLhjUAhiABBiKBRhDGJcFGNwEGN4EGN8E2AEDSMAZUOQLWNcNcAJ4AZABAJgBygGgAZIDqgEDMi0yuAEByAEA-AEBwgIKEAAYRxjWBBiwA8ICDRAAGIAEGIoFGLADGEPCAg4QABjkAhjWBBiwA9gBAcICExAuGIAEGIoFGMgDGLADGEPYAQLCAhYQLhiABBiKBRjUAhjIAxiwAxhD2AEC4gMEGAAgQYgGAZAGE7oGBggBEAEYCboGBggCEAEYCLoGBggDEAEYFA&sclient=gws-wiz-serp"
     },
     {
         id: 5,
         name: "Curlew",
         image: "https://images.pexels.com/photos/7467224/pexels-photo-7467224.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        description: "This is a project where I posted a Curlew",
-        gitRepo: "github.com",
+        description: "This is a placeholder where I posted a Curlew",
+        gitRepo: "https://www.github.com",
         website: "https://www.google.com/search?q=curlew+bird&sca_esv=585139827&ei=rx9hZeuGF6-wi-gP7-mgyAc&gs_ssp=eJzj4tTP1TcwzE7KMzdg9OJOLi3KSS1XSMosSgEAVyQHiw&oq=curlew&gs_lp=Egxnd3Mtd2l6LXNlcnAiBmN1cmxldyoCCBIyChAAGEcY1gQYsAMyChAAGEcY1gQYsAMyChAAGEcY1gQYsAMyChAAGEcY1gQYsAMyChAAGEcY1gQYsAMyChAAGEcY1gQYsAMyChAAGEcY1gQYsAMyChAAGEcY1gQYsAMyDRAAGIAEGIoFGLADGEMyDRAAGIAEGIoFGLADGEMyDRAAGIAEGIoFGLADGEMyDRAAGIAEGIoFGLADGEMyDhAAGOQCGNYEGLAD2AEBMg4QABjkAhjWBBiwA9gBATIOEAAY5AIY1gQYsAPYAQEyFhAuGIAEGIoFGNQCGMgDGLADGEPYAQIyExAuGIAEGIoFGMgDGLADGEPYAQIyExAuGIAEGIoFGMgDGLADGEPYAQIyExAuGIAEGIoFGMgDGLADGEPYAQJItxVQAFgAcAF4AZABAJgBAKABAKoBALgBAcgBAOIDBBgAIEGIBgGQBhO6BgYIARABGAm6BgYIAhABGAg&sclient=gws-wiz-serp"
     }
 ]
