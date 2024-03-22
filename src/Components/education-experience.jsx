e591b617cd72e7ea410032e51f2c8a52125fea8e
Lukas <lad_777@hotmail.com>
2024-03-22 12:56:28 +0000

    feat: more detail in education

--- src/Components/education-experience.jsx
+++ src/Components/education-experience.jsx
@@ -8,8 +8,9 @@ export default function EducationExperience() {
                     <br />
                     <ul>
                         <li>
-                            <h2>Boolean School of Code</h2>
-                            <p>2023/2024: 6 Month program covering both front-end development and back-end. Experience in group development and individual projects.</p>
+                            <h2>Boolean UK Software Development Bootcamp</h2>
+                            <p>2023/2024: A full time 6 month, 1000+ hour coding bootcamp, where we worked on all skills necessary to become a full-stack developer, I gained a passion for learning and growing in my code quality and tool kit of skills. With a background in outdoor pursuits and management I have learned many valuable skills that honed my experience with team building and working together to achieve a common goal.</p>
+                            <p>I enjoyed the experience of becoming a scrum master for a week and guiding my fellow collegues through some challenges on a team project we worked on. This I believe was a valuable skill in learning what working in an agile environment will be like.</p>
                         </li>
                     </ul>
                 </article>
