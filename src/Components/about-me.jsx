0adb044a7b55e1f021643b3d3987955973b54e78
Lukas <lad_777@hotmail.com>
2024-03-22 13:21:08 +0000

    feat: updated about me to look nicer

--- src/Components/about-me.jsx
+++ src/Components/about-me.jsx
@@ -9,10 +9,10 @@ export default function AboutMe() {
                     <h1>ABOUT ME:</h1>
                     <br />
                     <h2>Hello, my name is Lukas Allen Dembicki</h2>
-                    <p>I am a Full-Stack Developer with a passion to build scalable applications and websites.</p>
-                    <p>I am a Canadian that moved to the UK in 2020 and have been enjoying it very much. Myself and my wife live up in Scotland. </p>
-                    <p>I am an avid outdoorsman and also a nerd. I enjoy digging into puzzles and problem solving.</p>
-                    <p>After a long 9 years of working in lift operations management, I made a big move here and decided to persue a passion in technology. This naturally lead me to coding and I have spent the last 6 months and over 1000 hours learning with a coding bootcamp academy called Boolean.</p>
+                    <p>I am a Full-Stack Developer with a passion to build scalable applications and websites. <br /> <br />
+                        Originally from Canada, I moved to the UK in 2020 and have been enjoying it very much. Myself and my wife live up in Scotland. I am legally here on a spousal Visa through my wife and am on route for citizennship. <br /> <br />
+                        I have a passion and love being an avid outdoorsman and also a nerd. I enjoy digging into puzzles and problem solving. <br /> <br />
+                        After a long 9 years of working in lift operations management, I made the big move to the UK and decided to persue a passion in technology. This naturally lead me to coding and I have spent the last 6 months and over 1000 hours learning with a coding bootcamp academy called Boolean.</p>
                     <img src={lukas} className="lukasImage" alt="A photo of Lukas" />
                 </article>
             </section>
