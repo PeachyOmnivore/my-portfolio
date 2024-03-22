0f7052b08a6f31fe6a51e212b109540100d60e6c
Lukas <lad_777@hotmail.com>
2024-03-22 12:56:14 +0000

    feat: another paragraph to aboutme

--- src/Components/about-me.jsx
+++ src/Components/about-me.jsx
@@ -12,6 +12,7 @@ export default function AboutMe() {
                     <p>I am a Full-Stack Developer with a passion to build scalable applications and websites.</p>
                     <p>I am a Canadian that moved to the UK in 2020 and have been enjoying it very much. Myself and my wife live up in Scotland. </p>
                     <p>I am an avid outdoorsman and also a nerd. I enjoy digging into puzzles and problem solving.</p>
+                    <p>After a long 9 years of working in lift operations management, I made a big move here and decided to persue a passion in technology. This naturally lead me to coding and I have spent the last 6 months and over 1000 hours learning with a coding bootcamp academy called Boolean.</p>
                     <img src={lukas} className="lukasImage" alt="A photo of Lukas" />
                 </article>
             </section>
