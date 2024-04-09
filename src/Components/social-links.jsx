c1dda9d8b7f2f7759741b1906d0f10d8e521b632
Lukas <lad_777@hotmail.com>
2024-04-09 14:02:19 +0100

    docs: linkedIn link updated

--- src/Components/social-links.jsx
+++ src/Components/social-links.jsx
@@ -10,7 +10,7 @@ const navigate = useNavigate()
                     <a href="https://github.com/PeachyOmnivore"><img src='assets/svgs/GitHub.svg' className="socialsSvg" alt="GitHub"></img></a>
                 </li>
                  <li>
-                    <a href="https://www.linkedin.com/in/lukas-dembicki-4a4846251"><img className="socialsSvg" src='assets/svgs/LinkedIn.svg' alt="LinkedIn" /></a>
+                    <a href="https://www.linkedin.com/in/lukas-dembicki"><img className="socialsSvg" src='assets/svgs/LinkedIn.svg' alt="LinkedIn" /></a>
                 </li> 
                  <li>
                     <a href="https://www.instagram.com/peachyomnivore/"><img className="socialsSvg" src='assets/svgs/Instagram.svg' alt="Instagram" /></a>
